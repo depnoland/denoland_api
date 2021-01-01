35a42a9ead5f10d98931ae66cc0752daa17abb7f
Aki <71239005+AkiaCode@users.noreply.github.com>
2021-01-01 19:59:57 +0900

    Update baseURLs.ts

--- consts/baseURLs.ts
+++ consts/baseURLs.ts
@@ -7,3 +7,8 @@ export const API_BASEURL = 'https://api.deno.land'
  * baseURL of deno.land cdn
  */
 export const CDN_BASEURL = 'https://cdn.deno.land'
+
+/**
+ * baseURL of deno.land doc
+ */
+export const DOC_BASEURL = 'https://doc.deno.land'
